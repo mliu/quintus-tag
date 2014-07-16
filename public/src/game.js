62e9103eb49ce242439b2a5e181cfc516760f8c7
Michael Liu <michael.liu@rackspace.com>
2014-07-16 11:52:13 -0400

    Quintus singleplayer working

--- public/src/game.js
+++ public/src/game.js
@@ -4,27 +4,34 @@ var Q = Quintus({audioSupported: [ 'wav','mp3' ]})
       .enableSound()
       .controls().touch();
 
+Q.gravityY = 0;
+
 //Source other game objects
 var objectFiles = [
   './src/player'
-]
+];
 
 require(objectFiles, function () {
   Q.scene('arena', function (stage) {
     stage.insert(new Q.Repeater({ asset: '/images/background.png' }));
     stage.collisionLayer(new Q.TileLayer({ dataAsset: '/maps/arena.json', sheet: 'tiles' }));
 
+    var player = stage.insert(new Q.Player({ x: 100, y: 100 }));
+    stage.add('viewport').follow(player);
   });
 
   //Source game files
   var files = [
     '/images/background.png',
     '/images/tiles.png',
-    '/maps/arena.json'
+    '/maps/arena.json',
+    '/images/sprites.png',
+    '/images/sprites.json'
   ];
 
   Q.load(files.join(','), function () {
     Q.sheet('tiles', '/images/tiles.png', { tilew: 32, tileh: 32 });
-    Q.stageScene('arena');
+    Q.compileSheets('/images/sprites.png', '/images/sprites.json');
+    Q.stageScene('arena', 0);
   });
 });
--- public/src/player.js
+++ public/src/player.js
@@ -1,10 +1,30 @@
 require([], function () {
-  Q.Sprite.extend('Player', {
+  Q.Sprite.extend('Actor', {
+    init: function (p, hash) {
+      this._super(p, hash);
+
+      this.addEventListeners();
+    },
+  });
+
+  Q.Actor.extend('Player', {
     init: function (p) {
       this._super(p, {
+        sheet: 'player',
+        tagged: false
+      });
 
-      })
+      this.className = 'Player';
+      this.add('2d, platformerControls, animation');
     },
-
+    step: function (dt) {
+      if (Q.inputs['up']) {
+        this.p.vy = -200;
+      } else if (Q.inputs['down']) {
+        this.p.vy = 200;
+      } else if (!Q.inputs['down'] && !Q.inputs['up']) {
+        this.p.vy = 0;
+      }
+    }
   });
 });
