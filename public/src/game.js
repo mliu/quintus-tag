fd2d418c3b1f7a91790ddff906fd967e0ec512de
Michael Liu <michael.liu@rackspace.com>
2014-07-17 14:40:08 -0400

    working on tagging logic

--- public/src/game.js
+++ public/src/game.js
@@ -6,23 +6,19 @@ var Q = Quintus({audioSupported: [ 'wav','mp3' ]})
 
 Q.gravityY = 0;
 
-//Source other game objects
 var objectFiles = [
   './src/player'
 ];
 
 require(objectFiles, function () {
   Q.scene('arena', function (stage) {
-    stage.insert(new Q.Repeater({ asset: '/images/background.png' }));
     stage.collisionLayer(new Q.TileLayer({ dataAsset: '/maps/arena.json', sheet: 'tiles' }));
 
     var player = stage.insert(new Q.Player({ x: 100, y: 100 }));
     stage.add('viewport').follow(player);
   });
 
-  //Source game files
   var files = [
-    '/images/background.png',
     '/images/tiles.png',
     '/maps/arena.json',
     '/images/sprites.png',
--- public/src/player.js
+++ public/src/player.js
@@ -1,21 +1,40 @@
 require([], function () {
   Q.Sprite.extend('Actor', {
-    init: function (p, hash) {
-      this._super(p, hash);
-
-      this.addEventListeners();
-    },
-  });
-
-  Q.Actor.extend('Player', {
     init: function (p) {
       this._super(p, {
-        sheet: 'player',
         tagged: false
       });
 
-      this.className = 'Player';
+      this.add('animation');
+    }
+  });
+
+  Q.Sprite.extend('Player', {
+    init: function (p) {
+      this._super(p, {
+        sheet: 'player',
+        tagged: false,
+        invincible: false
+      });
       this.add('2d, platformerControls, animation');
+
+      this.addEventListeners();
+    },
+    addEventListeners: function () {
+      this.on('hit', function (collision) {
+        if (this.p.tagged && !collision.obj.p.tagged && !collision.obj.p.invincible) {
+          collision.obj.p.tagged = true;
+          this.p.tagged = false;
+          this.p.invincible = true;
+          this.p.opacity = 0.5;
+          this.p.speed = 400;
+          setTimeout(function () {
+            this.p.invincible = false;
+            this.p.opacity = 1;
+            this.p.speed = 200;
+          }, 3000);
+        }
+      });
     },
     step: function (dt) {
       if (Q.inputs['up']) {
